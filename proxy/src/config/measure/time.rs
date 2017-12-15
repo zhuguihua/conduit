9d411d80bb2444229bbf9eee57b2b0193ebf6600
Eliza Weisman <eliza@buoyant.io>
2017-12-15 15:55:20 -0800

    make time compile for now

--- proxy/src/config/measure/time.rs
+++ proxy/src/config/measure/time.rs
@@ -1,12 +1,9 @@
-use std::cmp::Ordering;
-use std::fmt;
-use std::str::FromStr;
+
 use std::marker::PhantomData;
-use ::config::{parse_number, ParseError};
 
 use super::Unit;
 
-#[derive(Copy, Clone, Debug, Eq, Ord)]
+#[derive(Copy, Clone, Debug)]
 pub struct Time<U>
 where
     U: Unit<Measure=Time<U>>,
