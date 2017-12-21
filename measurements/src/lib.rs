3e031524d9e87715b5e376cd827bd0099ca8587a
Eliza Weisman <eliza@buoyant.io>
2017-12-21 11:09:34 -0800

    move measurements to its own crate

---
+++ measurements/src/lib.rs
@@ -0,0 +1,240 @@
+use std::fmt;
+use std::num::ParseIntError;
+use std::error::Error;
+
+#[macro_export]
+macro_rules! mk_measure {
+    (struct $measure:ident($base_unit:ident))=> {
+        mk_measure!{ struct $measure($base_unit: u64) }
+    };
+    (pub struct $measure:ident($base_unit:ident)) => {
+        mk_measure!{ pub struct $measure($base_unit: u64) }
+    };
+    (pub struct $measure:ident($base_unit:ident:$repr:ty)) => {
+        #[derive(Copy, Clone, Debug)]
+        pub struct $measure<U>
+        where
+            U: $crate::Unit<Measure=$measure<U>>,
+        {
+            $base_unit: $repr,
+            unit: ::std::marker::PhantomData<U>,
+        }
+
+        mk_measure!{ @impl $measure($base_unit:$repr) }
+    };
+    (struct $measure:ident($base_unit:ident:$repr:ty)) => {
+        #[derive(Copy, Clone, Debug)]
+        struct $measure<U>
+        where
+            U: $crate::Unit<Measure=$measure<U>>,
+        {
+            $base_unit: $repr,
+            unit: std::marker::PhantomData<U>,
+        }
+
+        mk_measure!{ @impl $measure($base:$repr) }
+    };
+
+    (@impl $measure:ident($base_unit:ident:$repr:ty))=> {
+        impl_ops! { measure: $measure, base_unit: $base_unit: $repr =>
+            Add, add,
+            Sub, sub,
+            Div, div,
+            Mul, mul
+        }
+
+        impl<U> ::std::fmt::Display for $measure<U>
+        where 
+            U: $crate::Unit<Measure=$measure<U>, Repr=$repr>,
+        {
+            fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+                let float_value = 
+                    (self.$base_unit as f64) / (U::BASE_UNITS_PER_UNIT as f64);
+
+                write!(f,
+                    "{number} {name}{plural}",
+                    number=float_value,
+                    name=U::NAME,
+                    plural=if float_value == 1f64 { "" } else { "s" }
+                )
+            }
+        }
+
+        impl<U> From<$repr> for $measure<U>
+        where
+            U: $crate::Unit<Measure=$measure<U>, Repr=$repr>,
+        {
+            fn from(u: $repr) -> Self {
+                Self {
+                    $base_unit: u * U::BASE_UNITS_PER_UNIT,
+                    unit: PhantomData
+                }
+            }
+        }
+
+        impl<A> $measure<A> 
+        where 
+            A: $crate::Unit<Measure=$measure<A>, Repr=$repr>,
+        {
+            pub fn into<B>(self) -> $measure<B>
+            where
+                B: $crate::Unit<Measure=$measure<B>, Repr=$repr>,
+            {
+                $measure {
+                    $base_unit: self.$base_unit,
+                    unit: ::std::marker::PhantomData,
+                }
+            }
+        }
+
+        impl<A, B> PartialEq<$measure<B>> for $measure<A>
+        where   
+            A: $crate::Unit<Measure=$measure<A>, Repr=$repr>,
+            B: $crate::Unit<Measure=$measure<B>, Repr=$repr>,
+        {
+            fn eq(&self, rhs: &$measure<B>) -> bool{
+                self.$base_unit == rhs.$base_unit
+            }
+        }
+
+        impl<A, B> PartialOrd<$measure<B>> for $measure<A>
+        where   
+            A: $crate::Unit<Measure=$measure<A>, Repr=$repr>,
+            B: $crate::Unit<Measure=$measure<B>, Repr=$repr>,
+        {
+            fn partial_cmp(&self, rhs: &$measure<B>) 
+                          -> Option<::std::cmp::Ordering>
+            {
+                self.$base_unit.partial_cmp(&rhs.$base_unit)
+            }
+        }
+    }
+}
+
+macro_rules! impl_ops {
+    (
+        measure: $measure:ident, base_unit: $base_unit:ident: $repr:ty => 
+        $($trait:ident, $fun:ident ),+
+    ) => {
+        $(
+            impl<A, B> ::std::ops::$trait<$measure<B>> for $measure<A>
+            where   
+                A: $crate::Unit<Measure=$measure<A>, Repr=$repr>,
+                B: $crate::Unit<Measure=$measure<B>>,
+            {
+                type Output = Self;
+                fn $fun(self, rhs: $measure<B>) -> Self {
+                    Self {
+                        $base_unit: self.$base_unit.$fun(rhs.$base_unit),
+                        unit: PhantomData
+                    }
+                }
+            }
+        )+
+    }
+}
+#[macro_export]
+macro_rules! mk_units {
+    (
+        $measure:ident => 
+        $($name:ident, $short_name:ident, $long_name:ident, $base_per:expr),+
+    ) => {
+        mk_units! { $measure: u64 => 
+            $($name, $short_name, $long_name, $base_per),+
+        }
+    };
+    (
+        $measure:ident : $repr:ty => 
+        $($name:ident, $short_name:ident, $long_name:ident, $base_per:expr),+
+    ) => {
+        mk_units_inner! {
+            $measure : $repr, stringify!($measure) => 
+            $($name, $short_name, stringify!($long_name), $base_per),+
+        }
+    };
+}
+// factored out so we can stringify more eagerly for making doc comments.
+macro_rules! mk_units_inner {
+    (
+        $measure:ident : $repr:ty, $smeasure:expr => 
+        $($name:ident, $short_name:ident, $long_name:expr, $base_per:expr),+
+    ) => {
+        $(
+            pub type $short_name = $measure<$name>;
+
+            #[doc = "Unit representing a measurement of "] 
+            #[doc = $smeasure] #[doc = " in "] #[doc = $long_name] #[doc = "s."]
+            #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+            pub struct $name;
+
+            impl $crate::Unit for $name {
+                type Measure = $measure<$name>;
+                type Repr = $repr;
+                const NAME: &'static str = stringify!($long_name);
+                const SHORT_NAME: &'static str = stringify!($short_name);
+                const BASE_UNITS_PER_UNIT: $repr = $base_per;
+            }
+        )+
+    }
+} 
+
+/// Trait representing a measurement unit.
+pub trait Unit {
+    /// The corresponding type measurable with this unit.
+    type Measure;
+    /// Representation of a value measured by this unit.
+    /// 
+    /// This must be the same as the type used internally by 
+    /// `Self::Measure`.
+    // TODO: if `Measure` was a trait as well, the `Repr` type could go th
+    type Repr;
+    const NAME: &'static str;
+    const SHORT_NAME: &'static str;
+    const BASE_UNITS_PER_UNIT: Self::Repr;
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum MeasureError {
+    /// An invalid unit was used.
+    InvalidUnit,
+    InvalidNumber(ParseIntError),
+}
+
+pub mod storage;
+pub mod time;
+
+// ===== impl MeasureError =====
+
+impl Error for MeasureError {
+    fn description(&self) -> &str {
+        match *self {
+            MeasureError::InvalidUnit => "invalid unit",
+            MeasureError::InvalidNumber(_) => "invalid number",
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        if let MeasureError::InvalidNumber(ref err) = *self {
+            Some(err)
+        } else {
+            None
+        }
+    }
+}
+
+impl fmt::Display for MeasureError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            MeasureError::InvalidUnit => 
+                write!(f, "invalid unit found in string"),
+            MeasureError::InvalidNumber(ref err) => 
+                write!(f, "invalid number: {}", err),
+        }
+    }
+}
+
+impl From<ParseIntError> for MeasureError {
+    fn from(err: ParseIntError) -> Self {
+        MeasureError::InvalidNumber(err)
+    }
+}
---
+++ measurements/src/storage.rs
@@ -0,0 +1,146 @@
+use std::str::FromStr;
+use std::marker::PhantomData;
+
+use super::{Unit, MeasureError};
+
+
+mk_measure! { pub struct Storage(bytes) }
+
+mk_units!{ Storage =>
+    Bytes    , B , byte      , 1,
+    Kilobytes, KB, kilobyte  , 1_024,
+    Megabytes, MB, megabyte  , 1_048_576,
+    Gigabytes, GB, gigabyte  , 1_073_741_824
+}
+
+
+impl<U> FromStr for Storage<U> 
+where 
+    U: Unit<Measure=Storage<U>, Repr=u64>,
+{
+    type Err = MeasureError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // ignore leading + trailing whitespace.
+        let s = s.trim();
+        // XXX: this is kinda janky, what we really need is just like an 
+        //      iterator-oriented LL(1) parser...
+        let num_part = 
+            // XXX: also we dont support hex because `is_numeric` will gobble up 
+            //      the 'b's in *bytes...
+            s.trim_matches(|c: char| !c.is_digit(10))
+             .trim(); // trim again to skip any interstital whitespace.
+        let unit_part: String = 
+            s.trim_matches(|c: char| !c.is_alphabetic())
+            // NOTE: could save a string allocation by matching patterns 
+            //       like `"B" | "b"`, but that ends up looking much uglier
+            //       and this shouldn't be in the hot path...
+             .to_lowercase(); 
+        let num: u64 = num_part.parse().map_err(MeasureError::from)?;
+        match unit_part[..].trim() {
+            "b"  => Ok(Storage::<Bytes>::from(num).into::<U>()),
+            "kb" => Ok(Storage::<Kilobytes>::from(num).into::<U>()),
+            "mb" => Ok(Storage::<Megabytes>::from(num).into::<U>()),
+            "gb" => Ok(Storage::<Gigabytes>::from(num).into::<U>()),
+            _    => Err(MeasureError::InvalidUnit),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_zero_cost() {
+        use std::mem::size_of;
+        assert_eq!(
+            size_of::<Storage<Bytes>>(), size_of::<u64>()
+        );
+        assert_eq!(
+            size_of::<Storage<Kilobytes>>(), size_of::<u64>()
+        );
+        assert_eq!(
+            size_of::<Storage<Megabytes>>(), size_of::<u64>()
+        );
+        assert_eq!(
+            size_of::<Storage<Gigabytes>>(), size_of::<u64>()
+        );
+    }
+
+    #[test]
+    fn parsing_simple() {
+        assert_eq!(
+            "15 GB".parse::<Storage<Gigabytes>>()
+                    .expect("parse"),
+            Storage::<Gigabytes>::from(15)
+        );
+        assert_eq!(
+            "15 MB".parse::<Storage<Megabytes>>()
+                    .expect("parse"),
+            Storage::<Megabytes>::from(15)
+        );
+    }
+
+
+    #[test]
+    fn parsing_does_unit_conversions() {
+        assert_eq!(
+            "1024 B".parse::<Storage<Kilobytes>>()
+                    .expect("parse"),
+            Storage::<Kilobytes>::from(1)
+        );
+
+        assert_eq!(
+            "4096 KB".parse::<Storage<Kilobytes>>()
+                    .expect("parse"),
+            Storage::<Megabytes>::from(4)
+        );
+    }
+
+
+    #[test]
+    fn parsing_is_case_insensitive() {
+        assert_eq!(
+            "15 gb".parse::<Storage<Gigabytes>>()
+                   .expect("parse"),
+            Storage::<Gigabytes>::from(15)
+        );
+        assert_eq!(
+            "15 gB".parse::<Storage<Megabytes>>()
+                    .expect("parse"),
+            "15 GB".parse::<Storage<Gigabytes>>()
+                   .expect("parse"),
+        );
+        assert_eq!(
+            "15 Gb".parse::<Storage<Megabytes>>()
+                    .expect("parse"),
+            "15 gb".parse::<Storage<Gigabytes>>()
+                   .expect("parse"),
+        );
+    }
+
+    #[test]
+    fn parsing_handles_leading_and_trailing_whitespace() {
+        assert_eq!(
+            " 15 GB".parse::<Storage<Gigabytes>>()
+                   .expect("parse ' 15 GB'"),
+            Storage::<Gigabytes>::from(15)
+        );
+        assert_eq!(
+            "15GB".parse::<Storage<Gigabytes>>()
+                  .expect("parse '15GB'"),
+            Storage::<Gigabytes>::from(15)
+        );
+        assert_eq!(
+            "15 gb ".parse::<Storage<Gigabytes>>()
+                  .expect("parse '15 gb '"),
+            Storage::<Gigabytes>::from(15)
+        );
+        assert_eq!(
+            " 15 gb ".parse::<Storage<Gigabytes>>()
+                  .expect("parse ' 15 gb '"),
+            Storage::<Gigabytes>::from(15)
+        );
+
+    }
+}
