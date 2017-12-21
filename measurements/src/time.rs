b685335187c79fe761c7ca46d35c90de6ea0f8df
Eliza Weisman <eliza@buoyant.io>
2017-12-21 14:01:26 -0800

    Add {From/Into}<Duration> conversions for Time

--- measurements/src/time.rs
+++ measurements/src/time.rs
@@ -1,3 +1,6 @@
+use std::time::Duration;
+use super::Unit;
+
 mk_measure! { pub struct Time(ns: u64) }
 
 mk_units!{ Time: u64, ToTime =>
@@ -20,5 +23,71 @@ mk_units!{ Time: u64, ToTime =>
     },
     Hours {
         base: 3_600_000_000_000, long: hours, short: "hr", "h"
+    }
+}
+
+impl<U> Into<Duration> for Time<U> 
+where
+    U: Unit<Measure=Self, Repr=u64>,
+{
+    fn into(self) -> Duration {
+        Duration::from_millis(
+            self.as_unit::<Milliseconds>().into()
+        )
+    }
+}
+
+impl<U> From<Duration> for Time<U> 
+where
+    U: Unit<Measure=Self, Repr=u64>,
+{
+    fn from(dur: Duration) -> Self {
+        let seconds = dur.as_secs().seconds();
+        let nanos = (dur.subsec_nanos() as u64).nanoseconds();
+        (seconds + nanos).as_unit::<U>()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    quickcheck! {
+        fn microseconds_supports_unicode(u: u64) -> bool {
+            format!("{} µs", u)
+                .parse::<Time<Microseconds>>()
+                .expect("parse µs") == 
+            format!("{} us", u)
+                .parse::<Time<Microseconds>>()
+                .expect("parse us")
+        }
+
+        fn seconds_suffix_sec_or_s(u: u64) -> bool {
+            format!("{} sec", u)
+                .parse::<Time<Seconds>>()
+                .expect("parse sec") == 
+            format!("{} s", u)
+                .parse::<Time<Seconds>>()
+                .expect("parse s")
+        }
+
+        fn minutes_suffix_min_or_m(u: u64) -> bool {
+            format!("{} min", u)
+                .parse::<Time<Minutes>>()
+                .expect("parse min") == 
+            format!("{} m", u)
+                .parse::<Time<Minutes>>()
+                .expect("parse m")
+        }
+
+
+        fn hours_suffix_hr_or_h(u: u64) -> bool {
+            format!("{} hr", u)
+                .parse::<Time<Hours>>()
+                .expect("parse hr") == 
+            format!("{} h", u)
+                .parse::<Time<Hours>>()
+                .expect("parse h")
+        }
     }
 }
